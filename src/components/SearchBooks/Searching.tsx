496fc8305fa9a3ecb334314d8832c040535d623e
Ainsdev <ainsn1654@gmail.com>
2022-10-21 18:35:25 -0300

    missing deatils features

--- src/components/SearchBooks/Searching.tsx
+++ src/components/SearchBooks/Searching.tsx
@@ -33,7 +33,7 @@ const SearchingBook = () => {
                     </label>
                 </label>
                 <div className="input-group">
-                    <input value={searching[1]} onChange={(e) => setSearching(prev => [false, e.target.value])} type="text" placeholder="Search a title..." className="input input-accent sm:w-96" />
+                    <input value={searching[1]} onChange={(e) => setSearching(prev => [false, e.target.value])} type="text" placeholder="Search any title..." className="input input-accent sm:w-96" />
                     <button onClick={(e) => handleSearching(e)} className="btn btn-square">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                     </button>
